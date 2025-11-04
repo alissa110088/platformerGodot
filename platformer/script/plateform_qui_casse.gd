a274424e6350919a4590fa84615dd9ee6fbd8af7
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-11-04 18:29:19 +0100

    fix

--- platformer/script/plateform_qui_casse.gd
+++ platformer/script/plateform_qui_casse.gd
@@ -1,10 +1,14 @@
-extends StaticBody2D
+class_name PlatformCasse extends StaticBody2D
 @export var _anim: AnimationPlayer
-		
+@export var _timer: Timer
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	#if body is Player:
+	if body is not PlatformCasse:
+		_timer.start()
+
+
+func _on_timer_timeout() -> void:
 	_anim.play("PlatformCasse")
 	await _anim.animation_finished
 	queue_free()
