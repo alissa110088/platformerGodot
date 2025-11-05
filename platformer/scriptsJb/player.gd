baa16f92676519eb7ee4e8bf1414d684ae2bdaaa
Jean-Baptiste <deljeanbap@gmail.com>
2025-11-05 12:09:59 +0100

    Arrow

--- platformer/scriptsJb/player.gd
+++ platformer/scriptsJb/player.gd
@@ -5,6 +5,10 @@ var speed : float = 500.0
 var gravity : int = 1300
 @export var jump_impulse: float = 500.0
 
+var can_shoot: bool = true
+
+const projectile_scene: PackedScene = preload("res://Scene/arrow.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -24,9 +28,21 @@ func _physics_process(delta: float) -> void:
 	elif not is_on_floor():
 		velocity.y += gravity * delta
 		
+		
+	if Input.is_action_pressed("shoot") and can_shoot == true:
+		shoot()
+		can_shoot = false
+		
 	
 	move_and_slide()
 	
+	
+func shoot() -> void:
+	var projectile: Arrow = projectile_scene.instantiate()
+
+	projectile.global_position = $ArrowAnchor/Origin.global_position
+	get_parent().add_child(projectile)
+	projectile.global_rotation = $ArrowAnchor/Origin.global_rotation
 	
 	
 	
