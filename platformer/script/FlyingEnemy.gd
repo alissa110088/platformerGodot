c7c7913d3be471a5a64458f6a14cbc499f818272
Lu k <Lucas.kominiarz@laposte.net>
2025-11-05 16:00:15 +0100

    Flying enemy

--- platformer/script/FlyingEnemy.gd
+++ platformer/script/FlyingEnemy.gd
@@ -1,15 +1,42 @@
 class_name FlyingEnemy
 extends Enemy
 
-var _canShoot : bool = false
+@export var cooldown: Timer
+@export var anchor: Node2D
+
+var _canShoot : bool = true
+var _player: CharacterBody2D
+var _playerInTrigger : bool = false
 
 func _physics_process(delta: float) -> void:
-	pass
-
+	if !_playerInTrigger:
+		return
+	
+	if  _canShoot:
+		_canShoot = false
+		cooldown.start()
+		Shoot()
+		
+	anchor.look_at(_player.global_position)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	_canShoot = true
+	if body.is_in_group("Player"):
+		_player = body
+		_playerInTrigger = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		_player = body
+		_playerInTrigger = false
+		
+func _on_timer_timeout() -> void:
 	_canShoot = true
+	
+func Shoot() -> void:
+	$AnimationPlayer.play("AnimEnemy")
+	var projectileScene: PackedScene = preload("res://Scene/Projectile.tscn")
+	var projectile: CharacterBody2D = projectileScene.instantiate()
+	get_parent().add_child(projectile)
+	projectile.global_position = $Anchor/Marker2D.global_position
+	projectile.global_rotation = $Anchor/Marker2D.global_rotation
