4b1f5fcddc37452e51e92ca551b39a9fa9ab2971
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-11-05 12:09:51 +0100

    death zone et death du player

--- platformer/script/platforme_movante.gd
+++ platformer/script/platforme_movante.gd
@@ -1,18 +1,18 @@
 class_name MovingPlatform
 extends StaticBody2D
 
-@export var target1: Marker2D
-@export var target2: Marker2D
-@export var speed: float = 100.0
+@export var _target1: Marker2D
+@export var _target2: Marker2D
+@export var _speed: float = 100.0
 
 var _current_target: Marker2D
 
 func _ready():
-	_current_target = target1
+	_current_target = _target1
 
 func _physics_process(delta):
-	var dir = (_current_target.global_position - global_position).normalized()
-	global_position += dir * speed * delta
+	var _dir = (_current_target.global_position - global_position).normalized()
+	global_position += _dir * _speed * delta
 
 	if global_position.distance_to(_current_target.global_position) < 2.0:
-		_current_target = target2 if _current_target == target1 else target1
+		_current_target = _target2 if _current_target == _target1 else _target1
--- platformer/scriptsJb/player.gd
+++ platformer/scriptsJb/player.gd
@@ -1,32 +1,26 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-var direction: float = 0.0
-var speed : float = 500.0
-var gravity : int = 1300
+var _speed : float = 500.0
+var _gravity : int = 1300
+var _is_dead: bool = false
 @export var jump_impulse: float = 500.0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var _game_over: PackedScene
 	
 func _physics_process(delta: float) -> void:
-	velocity.x = Input.get_axis("move_left","move_right") * speed
+	if _is_dead:
+		return
 	
+	velocity.x = Input.get_axis("move_left","move_right") * _speed
 	
 	if is_on_floor() 	and Input.is_action_just_pressed("jump"):
-		print("test")
-		velocity.y -= jump_impulse + gravity * delta
+		velocity.y -= jump_impulse + _gravity * delta
 	elif not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += _gravity * delta
 		
+	move_and_slide()	
 	
-	move_and_slide()
-	
-	
-	
-	
+func die() -> void:
+	_is_dead = true
+	var scene = _game_over.instantiate()
+	owner.add_child(scene)	
+	scene.global_position = get_viewport_rect().size/2
