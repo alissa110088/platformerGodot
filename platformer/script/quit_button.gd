154137289cb16bb979c1d36629388195c6ab41ea
Romain <rovic77@gmail.com>
2025-11-05 16:17:56 +0100

    Pause Menu V1

---
+++ platformer/script/quit_button.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+
+func _on_mouse_entered() -> void:
+	$AnimationQuit.play("Quit")
+
+func _on_mouse_exited() -> void:
+	$AnimationQuit.play_backwards("Quit")
