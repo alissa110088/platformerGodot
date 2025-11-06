2705f81321a11686f7bb2eb17b75c63c95518c92
Lu k <Lucas.kominiarz@laposte.net>
2025-11-06 13:00:45 +0100

    Niveau 1 fini manque porte

--- platformer/script/BombEnemy.gd
+++ platformer/script/BombEnemy.gd
@@ -18,5 +18,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _explosion_damage_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy") or body.is_in_group("Player"):
 		body.TakeDamage(damages)
+		body.TakeDamage(damages)
 	
 	
