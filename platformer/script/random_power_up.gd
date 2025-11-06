8a13a9433b2eb909d1bab36c5386a8549df069cd
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-11-06 11:28:21 +0100

    mario truc

--- platformer/script/random_power_up.gd
+++ platformer/script/random_power_up.gd
@@ -1,11 +1,14 @@
 extends StaticBody2D
+@export var power_up : Array[PackedScene]
+@export var animation : AnimationPlayer
+@export var spawn_pos : Marker2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		animation.play("randomPowerUp")
+		await animation.animation_finished
+		var item = power_up.pick_random()
+		var instanciated_item: Node2D =  item.instantiate()
+		owner.add_child(instanciated_item)
+		instanciated_item.global_position = spawn_pos.global_position
+		
