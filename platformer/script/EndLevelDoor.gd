661cc035f82a4ba81b6ccf9c9f96f683c749659b
Lu k <Lucas.kominiarz@laposte.net>
2025-11-07 10:40:26 +0100

    script door modifs

--- platformer/script/EndLevelDoor.gd
+++ platformer/script/EndLevelDoor.gd
@@ -1,8 +1,12 @@
 extends StaticBody2D
 
+@export var pathScene : String 
 var _base_pos: Vector2 = Vector2(193.0, 146.0)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		Player.check_point_pos = _base_pos
-		get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
+		call_deferred("DeferredFunction")
+
+func DeferredFunction() -> void:
+	Player.check_point_pos = _base_pos
+	get_tree().change_scene_to_file(pathScene)
